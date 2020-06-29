b35985afe373769ded441ab6de2fe9a984787c89
shlomil <shlomil@reali.com>
2020-06-29 09:14:46 +0300

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AngularMaterialModule} from './material.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AddStudentComponent} from './components/add-student/add-student.component';
+import {EditStudentComponent} from './components/edit-student/edit-student.component';
+import {StudentsListComponent} from './components/students-list/students-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {ApiService} from './shared/api.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/shared/student.ts
@@ -0,0 +1,9 @@
+export class Student {
+  _id: string;
+  student_name: string;
+  student_email: string;
+  section: string;
+  subjects: Array<string>;
+  dob: Date;
+  gender: string;
+}
